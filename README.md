2fb76a78bfaa9c9de9bab3f4eadb463fa0470f78
u-menya-sha-v-tele-gramm <125315707+u-menya-sha-v-tele-gramm@users.noreply.github.com>
2024-12-25 20:54:42 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# PKS
+Практика 11
+
+Маркин Егор
+
+ЭФБО-02-22
+
+Инициализируем supabase
+
+![image](https://github.com/user-attachments/assets/76352c0b-d6c1-450b-a725-7687a60ac87a)
+
+Добавлено несколько страниц для корректной работы профиля: страница профился, авторизация, регистрация
+
+![image](https://github.com/user-attachments/assets/ce70143f-93ec-4a03-943b-cd58f53aaf32)
+![image](https://github.com/user-attachments/assets/4ab9bc98-3877-4b4a-a9ea-963dbb60362f)
+![image](https://github.com/user-attachments/assets/b0ebb3ed-bb60-4a33-8fee-f423332432b7)
+
+
+Вот наш пользователь с почтой snasti@mail.com
+![Снимок экрана 2024-12-15 164029](https://github.com/user-attachments/assets/e6f8921a-5680-4d05-a9a8-d6250eb78ba4)
+
---
+++ lib/main.dart
@@ -0,0 +1,283 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'product_box.dart';
+import 'product_detail_page.dart';
+import 'account_page.dart';
+import 'shopping_cart_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'LoginPage.dart';
+
+void main() async {
+  // Убедитесь, что все плагины инициализированы
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Инициализация Supabase
+  await Supabase.initialize(
+    url: 'https://hvtufmrmhwcqqhlkradu.supabase.co', // Замените на ваш URL Supabase
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dHVmbXJtaHdjcXFobGtyYWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNjUxMTIsImV4cCI6MjA0OTg0MTExMn0.kb-GTWVs8Alpl6GUZVs5-POYFxeIYfCgstVwmUZGmxY', // Замените на ваш Anon Key Supabase
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Демо Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  List<Product> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadProducts();
+  }
+
+  List<Product> get cartProducts => products.where((product) => product.isInCart).toList();
+
+  Future<void> loadProducts() async {
+    try {
+      final response = await http.get(Uri.parse('http://localhost:8080/products'));
+
+      print('Статус-код ответа: ${response.statusCode}');
+
+      if (response.statusCode == 200) {
+        // Пробуем декодировать тело ответа как UTF-8
+        final decodedResponse = utf8.decode(response.bodyBytes);
+        final List<dynamic> data = json.decode(decodedResponse);
+        print(data);
+        setState(() {
+          products = data.map((item) => Product.fromJson(item)).toList();
+        });
+      } else {
+        throw Exception('Не удалось загрузить продукты: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Ошибка при загрузке продуктов: $e');
+    }
+  }
+
+  Future<void> toggleFavorite(int productId) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/toggle-favorite?id=$productId'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Обработка успешного ответа
+      print('Изменение состояния IsLiked успешно');
+    } else {
+      // Обработка ошибки
+      print('Ошибка при изменении состояния IsLiked: ${response.body}');
+    }
+  }
+
+  Future<void> toggleCart(int productId) async {
+    final response = await http.post(
+      Uri.parse('http://localhost:8080/toggle-cart?id=$productId'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Обработка успешного ответа
+      print('Изменение состояния IsInCart успешно');
+    } else {
+      // Обработка ошибки
+      print('Ошибка при изменении состояния IsInCart: ${response.body}');
+    }
+  }
+
+  void removeProduct(Product product) {
+    setState(() {
+      products.remove(product);
+    });
+  }
+
+  void addProduct(Product product) {
+    setState(() {
+      products.add(product);
+    });
+  }
+
+void _onItemTapped(int index) {
+  if (index == 2) {
+    if (Supabase.instance.client.auth.currentUser  == null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => AccountPage(),
+        ),
+      );
+    }
+  } else {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+  @override
+  Widget build(BuildContext context) {
+    List<Product> displayedProducts;
+
+    if (_selectedIndex == 1) {
+      displayedProducts = products.where((product) => product.isLiked).toList();
+    } else {
+      displayedProducts = products;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedIndex == 0 ? 'Главная страница' : 'Избранное'),
+      ),
+      body: displayedProducts.isEmpty
+          ? const Center(child: Text('Нет товаров для отображения'))
+          : ListView.builder(
+              padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+              itemCount: displayedProducts.length,
+              itemBuilder: (context, index) {
+                final product = displayedProducts[index];
+                return ProductBox(
+                  name: product.name,
+                  description: product.description,
+                  price: product.price,
+                  imageUrl: product.imageUrl,
+                  isLiked: product.isLiked,
+                  isInCart: product.isInCart,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductDetailPage(
+                          product: product,
+                          onDelete: () => removeProduct(product),
+                        ),
+                      ),
+                    );
+                  },
+                  onToggleFavorite: () async {
+                    setState(() {
+                      product.toggleFavorite();
+                    });
+                    await toggleFavorite(product.id);
+                  },
+                  onToggleCart: () async {
+                    setState(() {
+                      product.toggleCart();
+                    });
+                    await toggleCart(product.id);
+                  },
+                );
+              },
+            ),
+      floatingActionButton: _selectedIndex == 0
+          ? FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ShoppingCartPage(),
+                  ),
+                );
+              },
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.shopping_cart),
+            )
+          : null,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Главная страница',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Избранное',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Аккаунт',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class Product {
+  final int id; // Добавляем поле id
+  final String name;
+  final String description;
+  final int price;
+  final String imageUrl;
+  bool isLiked;
+  bool isInCart;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isLiked = false,
+    this.isInCart = false,
+    this.quantity = 1,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      price: json['price'],
+      imageUrl: json['imageUrl'],
+      isLiked: json['IsLiked'] ?? false,
+      isInCart: json['isInCart'] ?? false,
+      quantity: json['quantity'] ?? 1,
+    );
+  }
+
+  void toggleFavorite() {
+    isLiked = !isLiked;
+  }
+
+  void toggleCart() {
+    isInCart = !isInCart;
+  }
+}
